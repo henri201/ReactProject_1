4b450afa2bb3b219c6df636f64fee54bf290d0b4
hpriim2gi <henri.priim2gi@hotmail.com>
2024-02-28 11:27:07 +0200

    using teh spread operator

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,14 +1,15 @@
-import CoreConcept from './CoreConcept.jsx';
-import { CORE_CONCEPTS } from '../data';
-
+import CoreConcept from "./CoreConcept.jsx";
+import { CORE_CONCEPTS } from "../data";
 
 export default function CoreConcepts() {
-    return (
-        <section id="core-concepts">
-          <h2>core concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (<CoreConcept key={conceptItem.title} {...conceptItem} />))}
-          </ul>
-        </section> 
-    );
+  return (
+    <section id="core-concepts">
+      <h2>core concepts</h2>
+      <ul>
+        {CORE_CONCEPTS.map((conceptItem) => (
+          <CoreConcept key={conceptItem.title} {...conceptItem} />
+        ))}
+      </ul>
+    </section>
+  );
 }
--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { EXAMPLES } from "../data";
-import TabButton from "./TabButton";
+import { EXAMPLES } from "../data.js";
+import TabButton from "./TabButton.jsx";
+import Section from "./Section.jsx"
 
 export default function Examples() {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -24,30 +25,29 @@ export default function Examples() {
   }
 
   return (
-    <section id="examples">
-      <h2>Examples</h2>
+    <Section title="Examples" id="examples">
       <menu>
         <TabButton
           isSelected={selectedTopic === "components"}
-          onSelect={() => handleSelect("components")}
+          onClick={() => handleSelect("components")}
         >
           Components
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "jsx"}
-          onSelect={() => handleSelect("jsx")}
+          onClick={() => handleSelect("jsx")}
         >
           JSX
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "props"}
-          onSelect={() => handleSelect("props")}
+          onClick={() => handleSelect("props")}
         >
           Props
         </TabButton>
-        <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+        <TabButton onClick={() => handleSelect("state")}>State</TabButton>
       </menu>
       {tabContent}
-    </section>
+    </Section>
   );
 }
